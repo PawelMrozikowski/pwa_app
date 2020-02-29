5679cc49e0d02f15f105b6b41ffdbccb1d0f4be7
Pawel Mrozikowski <mrozik.p@gmail.com>
2020-02-29 17:29:59 +0100

    feta:date

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,73 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+const time = document.querySelector('.date--js');
 
+const counter = document.querySelector('.glass__counter--js');
+
+const buttonAdd = document.querySelector('.glass__button-add--js');
+
+const buttonRemove = document.querySelector('.glass__button-remove--js');
+
+const date = new Date() .toISOString() .slice (0, 10);
+console.log(date);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('czas');
+  time.innerHTML = `date: ${date}`;
+})
+
+  
+if(!localStorage.getItem(date)){
+  localStorage.setItem(date, 0);
+  counter.innerHTML = '0';  
+} else {
+  counter.innerHTML = localStorage.getItem(date);
+}
+
+buttonAdd.addEventListener('click', (e) => {
+  console.log('dziala');
+  counter.innerHTML = parseInt(counter.innerHTML)+1;
+  localStorage.setItem(date, parseInt(counter.innerHTML));
+  
+  
+})
+
+buttonRemove.addEventListener('click', (e) => {
+  if(counter.innerHTML>0){
+  localStorage.setItem(date, parseInt(localStorage.getItem(date))-1);
+  counter.innerHTML = localStorage.getItem(date);
+}});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if (!localStorage.getItem(date)){
+  //localStorage.setItem(date, 0);
+  counter.innerHTML = '0';
+} else {
+  counter.innerHTML = localStorage.getItem(date);
+}
+buttonAdd.addEventListener('click', (e)=> {
+  console.log(`dziala`);
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+  
+  
+});
+
+buttonRemove.addEventListener('click', (e)=> {
+  if(counter.innerHTML>0){
+  counter.innerHTML = parseInt(counter.innerHTML) -1;
+}}); */
